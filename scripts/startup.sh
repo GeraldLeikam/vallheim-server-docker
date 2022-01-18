0bd97f9b92b4fec9452b7e962ca52e52482acb48
Gerald Leikam <gerald.leikam@aol.com>
2022-01-18 05:55:47 +0100

    modified:   Dockerifle 	modified:   docker-compose/docker-compose.yml 	modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,12 +2,12 @@
 SCRIPT_SERVER_NAME="${SERVER_NAME:-'Default Valheim Server'}"
 SCRIPT_WORlD_NAME="${WORLD_NAME:-'DefaultValheimWorld'}"
 
-/usr/games/steamcmd +force_install_dir /valheim-server +login anonymous +app_update 896660 validate +exit
+/usr/games/steamcmd +force_install_dir /valheim-server/server +login anonymous +app_update 896660 validate +exit
 export templdpath=$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/valheim-server/linux64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/valheim-server/server/linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
 export LD_LIBRARY_PATH=$templdpath
 
 echo "Starting server PRESS CTRL-C to exit"
 
-/valheim-server/valheim_server.x86_64 -name \"$SCRIPT_SERVER_NAME\" -port 2456 -world \"$SCRIPT_WORlD_NAME\" -savedir /valheim-server/save
+/valheim-server/server/valheim_server.x86_64 -name \"$SCRIPT_SERVER_NAME\" -port 2456 -world \"$SCRIPT_WORlD_NAME\" -savedir "/valheim-server/save"
