f1d2578327fa2f80ca0b5417d2ff273c541935ee
Gerald Leikam <gerald.leikam@aol.com>
2022-01-18 17:31:45 +0100

    modified:   docker-compose/docker-compose.yml 	modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,9 +3,22 @@ DEBUG=true
 SCRIPT_SERVER_NAME="${SERVER_NAME:-'Default Valheim Server'}"
 SCRIPT_WORlD_NAME="${WORLD_NAME:-'DefaultValheimWorld'}"
 
-if [ $DEBUG==true ]; then
+
+if [ PUBLIC==true ]
+then
+  SCRIPT_PUBLIC=1
+elif [ PUBLIC==false]
+then
+  SCRIPT_PUBLIC=0
+else
+  SCRIPT_PUBLIC=1
+fi
+
+if [ $DEBUG==true ]
+then
   echo $SCRIPT_SERVER_NAME
   echo $SCRIPT_WORlD_NAME
+  echo $SCRIPT_PUBLIC
 fi
 
 /usr/games/steamcmd +force_install_dir /valheim-server/server +login anonymous +app_update 896660 validate +exit
@@ -15,7 +28,7 @@ export SteamAppId=892970
 
 echo "Starting server PRESS CTRL-C to exit"
 
-/valheim-server/server/valheim_server.x86_64 -name "${SCRIPT_SERVER_NAME}" -port 2456 -world "${SCRIPT_WORlD_NAME}" -password "secret" -savedir /valheim-server/save
+/valheim-server/server/valheim_server.x86_64 -name "${SCRIPT_SERVER_NAME}" -port 2456 -world "${SCRIPT_WORlD_NAME}" -password "secret" -savedir /valheim-server/save -public 1
 export LD_LIBRARY_PATH=$templdpath
 
 while true
