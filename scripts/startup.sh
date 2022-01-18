bf17d6b48fc4723246d479d34e24b14c8e2368c1
Gerald Leikam <gerald.leikam@aol.com>
2022-01-18 04:33:25 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+if [[ -z "${SERVER_NAME}" ]]; then
+  SERVER_NAME="My Default Valheim Server. Please rename me."
+else
+  SERVER_NAME="${SERVER_NAME}"
+fi
+
+if [[ -z "${WORLD_NAME}" ]]; then
+  WOLRD_NAME="DefaultGeneratedWorld"
+else
+  WORLD_NAME="${SERVER_NAME}"
+fi
+
 /usr/games/steamcmd +login anonymous +force_install_dir /valheim-server +app_update 896660 validate +exit
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/valheim-server/linux64:$LD_LIBRARY_PATH
@@ -6,6 +18,6 @@ export SteamAppId=892970
 
 echo "Starting server PRESS CTRL-C to exit"
 
-/valheim-server/valheim_server.x86_64 -name "Drezael's Test Server *** Do not join" -port 2456 -world "Test Server *** Do not join" -password "secret"
+/valheim-server/valheim_server.x86_64 -name $SERVER_NAME -port 2456 -world $WOLRD_NAME
 
 export LD_LIBRARY_PATH=$templdpath
