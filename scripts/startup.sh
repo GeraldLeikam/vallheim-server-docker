c84249d43b3b23607869e1accdad1a1f44e10bf7
Gerald Leikam <gerald.leikam@aol.com>
2022-01-18 03:15:22 +0100

    new file:   Dockerifle 	new file:   apps/steamcmd/usr/games/steamcmd 	new file:   apps/steamcmd/usr/share/doc/steamcmd/changelog.Debian.gz 	new file:   apps/steamcmd/usr/share/doc/steamcmd/copyright 	new file:   apps/steamcmd/usr/share/doc/steamcmd/steamcmdcommands.txt.gz 	new file:   apps/steamcmd/usr/share/lintian/overrides/steamcmd 	new file:   apps/steamcmd/usr/share/man/man6/steamcmd.6.gz 	new file:   scripts/startup.sh

---
+++ scripts/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+/usr/games/steamcmd +login anonymous +force_install_dir /valheim-server +app_update 896660 validate +exit
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/valheim-server/linux64:$LD_LIBRARY_PATH
+export SteamAppId=892970
+
+echo "Starting server PRESS CTRL-C to exit"
+
+/valheim-server/valheim_server.x86_64 -name "*** Test Server *** Do not join" -port 2456 -world "*** Test Server *** Do not join" -password "secret"
+
+export LD_LIBRARY_PATH=$templdpath
