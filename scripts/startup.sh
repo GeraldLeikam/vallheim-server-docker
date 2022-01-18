8f2a3188845382c73cb6daa5c559917c525f97d3
Gerald Leikam <gerald.leikam@aol.com>
2022-01-18 11:07:11 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -17,3 +17,8 @@ export LD_LIBRARY_PATH=$templdpath
 echo "Starting server PRESS CTRL-C to exit"
 
 /valheim-server/server/valheim_server.x86_64 -name "${SCRIPT_SERVER_NAME}" -port 2456 -world "${SCRIPT_WORlD_NAME}" -savedir "/valheim-server/save"
+
+while true
+do
+  sleep 1
+done
