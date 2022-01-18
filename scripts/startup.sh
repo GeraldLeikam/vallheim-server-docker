13f701be371e5436b68072e51ec8629a0ebbe2e4
Gerald Leikam <gerald.leikam@aol.com>
2022-01-18 11:57:40 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -12,11 +12,11 @@ fi
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/valheim-server/server/linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
-export LD_LIBRARY_PATH=$templdpath
 
 echo "Starting server PRESS CTRL-C to exit"
 
-/valheim-server/server/valheim_server.x86_64 -name "Drezaels Testserver" -port 2456 -world "TestWorld" -savedir "/valheim-server/save"
+/valheim-server/server/valheim_server.x86_64 -name "Drezaels Testserver" -port 2456 -world "TestWorld" -password "secret"
+export LD_LIBRARY_PATH=$templdpath
 
 while true
 do
