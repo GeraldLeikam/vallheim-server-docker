98c57b1d456d391df36004e30c5d13e86e5d6da8
Gerald Leikam <gerald.leikam@aol.com>
2022-01-19 00:44:06 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,33 +5,39 @@ DEFAULT_SERVER_PORT=2456
 DEFAULT_WORLD_NAME="DefaultGeneratedWorld"
 DEFAULT_SERVER_PASSWORD="secret"
 DEFAULT_SAVE_DIR=/valheim-server/save
+DEFAULT_PUBLIC=1
 ARGUMENT_STRING=""
 if [ -n "${SERVER_NAME}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-name \"${SERVER_NAME}\" "; else ARGUMENT_STRING="${ARGUMENT_STRING}-name \"${DEFAULT_SERVER_NAME}\" "; fi
 if [ -n "${SERVER_PORT}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-port ${SERVER_PORT} "; else ARGUMENT_STRING="${ARGUMENT_STRING}-port ${DEFAULT_SERVER_PORT} "; fi
 if [ -n "${WORLD_NAME}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-world \"${WORLD_NAME}\" "; else ARGUMENT_STRING="${ARGUMENT_STRING}-world \"${DEFAULT_WORLD_NAME}\" "; fi
 if [ -n "${SERVER_PASSWORD}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-password \"${SERVER_PASSWORD}\" "; else ARGUMENT_STRING="${ARGUMENT_STRING}-password \"${DEFAULT_SERVER_PASSWORD}\" "; fi
 ARGUMENT_STRING="${ARGUMENT_STRING}-savedir ${DEFAULT_SAVE_DIR} "
+
 SCRIPT_SERVER_NAME="${SERVER_NAME:-'Default Valheim Server'}"
 SCRIPT_WORlD_NAME="${WORLD_NAME:-'DefaultValheimWorld'}"
-
-echo "-------------------------------------------------------------------------------------------------------------------"
-echo $ARGUMENT_STRING
-echo "-------------------------------------------------------------------------------------------------------------------"
 
 if [ -n "${PUBLIC}" ];
 then
   if [ "$(echo $PUBLIC | tr A-Z a-z)" = "true" ];
   then
     SCRIPT_PUBLIC=1
+    ARGUMENT_STRING="${ARGUMENT_STRING}-public 1 "
   elif [ "$(echo $PUBLIC | tr A-Z a-z)" = "false" ];
   then
     SCRIPT_PUBLIC=0
+    ARGUMENT_STRING="${ARGUMENT_STRING}-public 0 "
   else
     SCRIPT_PUBLIC=1
+    ARGUMENT_STRING="${ARGUMENT_STRING}-public 1 "
   fi
 else
   SCRIPT_PUBLIC=1
+  ARGUMENT_STRING="${ARGUMENT_STRING}-public ${DEFAULT_PUBLIC} "
 fi
+
+echo "-------------------------------------------------------------------------------------------------------------------"
+echo $ARGUMENT_STRING
+echo "-------------------------------------------------------------------------------------------------------------------"
 
 if [ -n "${AUTOUPDATE}" ];
 then
