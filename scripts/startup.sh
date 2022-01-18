5a81478295b1d982ebacc8bf68e1126009034590
Gerald Leikam <gerald.leikam@aol.com>
2022-01-19 00:57:03 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,7 +3,7 @@ DEBUG=true
 DEFAULT_SERVER_NAME="Default Valheim Server"
 DEFAULT_SERVER_PORT=2456
 DEFAULT_WORLD_NAME="DefaultGeneratedWorld"
-DEFAULT_SERVER_PASSWORD="secret"
+DEFAULT_SERVER_PASSWORD=secret
 DEFAULT_SAVE_DIR=/valheim-server/save
 DEFAULT_PUBLIC=1
 ARGUMENT_STRING=""
@@ -13,31 +13,20 @@ if [ -n "${WORLD_NAME}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-world \"${WO
 if [ -n "${SERVER_PASSWORD}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-password \"${SERVER_PASSWORD}\" "; else ARGUMENT_STRING="${ARGUMENT_STRING}-password \"${DEFAULT_SERVER_PASSWORD}\" "; fi
 ARGUMENT_STRING="${ARGUMENT_STRING}-savedir ${DEFAULT_SAVE_DIR} "
 
-SCRIPT_SERVER_NAME="${SERVER_NAME:-'Default Valheim Server'}"
-SCRIPT_WORlD_NAME="${WORLD_NAME:-'DefaultValheimWorld'}"
-
 if [ -n "${PUBLIC}" ];
 then
   if [ "$(echo $PUBLIC | tr A-Z a-z)" = "true" ];
   then
-    SCRIPT_PUBLIC=1
     ARGUMENT_STRING="${ARGUMENT_STRING}-public 1 "
   elif [ "$(echo $PUBLIC | tr A-Z a-z)" = "false" ];
   then
-    SCRIPT_PUBLIC=0
     ARGUMENT_STRING="${ARGUMENT_STRING}-public 0 "
   else
-    SCRIPT_PUBLIC=1
-    ARGUMENT_STRING="${ARGUMENT_STRING}-public 1 "
+    ARGUMENT_STRING="${ARGUMENT_STRING}-public ${DEFAULT_PUBLIC} "
   fi
 else
-  SCRIPT_PUBLIC=1
   ARGUMENT_STRING="${ARGUMENT_STRING}-public ${DEFAULT_PUBLIC} "
 fi
-
-echo "-------------------------------------------------------------------------------------------------------------------"
-echo $ARGUMENT_STRING
-echo "-------------------------------------------------------------------------------------------------------------------"
 
 if [ -n "${AUTOUPDATE}" ];
 then
@@ -60,6 +49,7 @@ then
   echo $SCRIPT_WORlD_NAME
   echo $SCRIPT_PUBLIC
   echo $SCRIPT_AUTOUPDATE
+  echo $ARGUMENT_STRING
 fi
 
 if [ $SCRIPT_AUTOUPDATE = "true" ];
@@ -73,10 +63,6 @@ export SteamAppId=892970
 
 echo "Starting server PRESS CTRL-C to exit"
 
-ARGUMENTS_STRING="-name \"${SCRIPT_SERVER_NAME}\" -port 2456 - world \"${SCRIPT_WORlD_NAME}\" -password \"secret\" -savedir /valheim-server/save -public ${SCRIPT_PUBLIC}"
-echo $ARGUMENT_STRING
-
-#/valheim-server/server/valheim_server.x86_64 -name "${SCRIPT_SERVER_NAME}" -port 2456 -world "${SCRIPT_WORlD_NAME}" -password "secret" -savedir /valheim-server/save -public $SCRIPT_PUBLIC
 /valheim-server/server/valheim_server.x86_64 "${ARGUMENT_STRING}"
 export LD_LIBRARY_PATH=$templdpath
 
