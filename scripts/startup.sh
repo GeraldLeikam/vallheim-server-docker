5e9ea53153655b8354964ae5f3e79e4b3ed3fcd8
Gerald Leikam <gerald.leikam@aol.com>
2022-01-18 05:20:01 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 if [[ -z "${SERVER_NAME}" ]]; then
-  SERVER_NAME="My Default Valheim Server. Please rename me."
+  SCRIPT_SERVER_NAME="My Default Valheim Server. Please rename me."
 else
-  SERVER_NAME="${SERVER_NAME}"
+  SCRIPT_SERVER_NAME="${SERVER_NAME}"
 fi
 
 if [[ -z "${WORLD_NAME}" ]]; then
-  WOLRD_NAME="DefaultGeneratedWorld"
+  SCRIPT_WORlD_NAME="DefaultGeneratedWorld"
 else
-  WORLD_NAME="${SERVER_NAME}"
+  SCRIPT_WORLD_NAME="${WORLD_NAME}"
 fi
 
 /usr/games/steamcmd +force_install_dir /valheim-server +login anonymous +app_update 896660 validate +exit
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/valheim-server/linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
+export LD_LIBRARY_PATH=$templdpath
 
 echo "Starting server PRESS CTRL-C to exit"
 
-/valheim-server/valheim_server.x86_64 -name \"$SERVER_NAME\" -port 2456 -world \"$WOLRD_NAME\"
-
-export LD_LIBRARY_PATH=$templdpath
+/valheim-server/valheim_server.x86_64 -name \"$SCRIPT_SERVER_NAME\" -port 2456 -world \"$SCRIPT_WORlD_NAME\"
