644653b0de23c1f445b218f8190ffffd9cc6b960
Gerald Leikam <gerald.leikam@aol.com>
2022-01-19 05:25:27 +0100

    modified:   Dockerifle 	modified:   scripts/game_mode_installer.sh

--- scripts/game_mode_installer.sh
+++ scripts/game_mode_installer.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 wget -v https://valheim.thunderstore.io/package/download/denikson/BepInExPack_Valheim/5.4.1700/ -O /tmp/denikson-BepInExPack_Valheim-5.4.1700.zip
+unzip /tmp/denikson-BepInExPack_Valheim-5.4.1700.zip -d /tmp/BepInEx
+cp -rv /tmp/BepInEx/BepInExPack_Valheim/* /valheim-server/BepInEx
+rm -v /tmp/denikson-BepInExPack_Valheim-5.4.1700.zip
+rm -rv /tmp/BepInEx
