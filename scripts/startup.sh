21fcdb4283f154f0e070755e3b064ac93dab333a
Gerald Leikam <gerald.leikam@aol.com>
2022-01-19 01:04:41 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -10,7 +10,7 @@ ARGUMENT_STRING=""
 if [ -n "${SERVER_NAME}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-name \"${SERVER_NAME}\" "; else ARGUMENT_STRING="${ARGUMENT_STRING}-name \"${DEFAULT_SERVER_NAME}\" "; fi
 if [ -n "${SERVER_PORT}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-port ${SERVER_PORT} "; else ARGUMENT_STRING="${ARGUMENT_STRING}-port ${DEFAULT_SERVER_PORT} "; fi
 if [ -n "${WORLD_NAME}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-world \"${WORLD_NAME}\" "; else ARGUMENT_STRING="${ARGUMENT_STRING}-world \"${DEFAULT_WORLD_NAME}\" "; fi
-if [ -n "${SERVER_PASSWORD}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-password \"${SERVER_PASSWORD}\" "; else ARGUMENT_STRING="${ARGUMENT_STRING}-password \"${DEFAULT_SERVER_PASSWORD}\" "; fi
+if [ -n "${SERVER_PASSWORD}" ]; then ARGUMENT_STRING="${ARGUMENT_STRING}-password ${SERVER_PASSWORD} "; else ARGUMENT_STRING="${ARGUMENT_STRING}-password ${DEFAULT_SERVER_PASSWORD} "; fi
 ARGUMENT_STRING="${ARGUMENT_STRING}-savedir ${DEFAULT_SAVE_DIR} "
 
 if [ -n "${PUBLIC}" ];
@@ -68,5 +68,6 @@ export LD_LIBRARY_PATH=$templdpath
 
 while true
 do
-  sleep 1
+  echo "I'am dead. Please kill me"
+  sleep 30
 done
