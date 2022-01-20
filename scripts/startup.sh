183ffc3487be4166e8b3699383c1b442f16e5d48
Gerald Leikam <gerald.leikam@aol.com>
2022-01-20 01:37:00 +0100

    modified:   scripts/startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -38,6 +38,9 @@ install_mods ()
     IFS='/' read -r -a modarray <<< $mod
     filename="${modarray[${#modarray[@]} - 1]}"
     wget "${mod}" -O "/tmp/${filename}"
+    unzip "/tmp/${filename}" -d "/tmp/current_mod"
+    cp -r "/tmp/current_mod/plugins/*" "/valheim-server/server/BepInEx/plugins"
+    rm -r "/tmp/current_mod"
   done
 }
 
